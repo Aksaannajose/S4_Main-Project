98a6828ecc5616ea1caf53420904f864cfd5d9a7
Aksaannajose <77113062+Aksaannajose@users.noreply.github.com>
2022-09-19 16:28:33 +0530

    Delete PLACEMENT directory

--- PLACEMENT/delete1.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$id=$_GET['delete'];
-	include('db.php');
-	mysqli_query($conn,"delete from `college_registration_table` where id='$id'");
-	// echo "<script>alert('Deleted Successfully');</script>";
-	// sleep(10);
-	header('location:admin admin college.php?alertmsg="Deleted Successfully"');
-?>
--- PLACEMENT/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start ( ) ;
-if ( ! isset ($_SESSION['	login_id' ] ) ) {
-$msg="invalid user";
-                         header ( ' location : signin.php?msg='  .$msg);
-						 }
-						 ?>
